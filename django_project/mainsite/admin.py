b4e64e785658bd77ed32baa59a10823f667aeeea
zqq135 <3528993306@qq.com>
2026-05-26 17:54:59 +0800

    Delete django_project directory

--- django_project/mainsite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import GameRecord
-
-
-@admin.register(GameRecord)
-class GameRecordAdmin(admin.ModelAdmin):
-    list_display = ["theme_name", "difficulty", "score", "moves", "time_seconds", "completed_at"]
-    list_filter = ["theme_name", "difficulty"]
-    search_fields = ["theme_name"]
